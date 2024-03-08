ca38b7c2dec72c5ead7f70a0e663c1f7c4c35fa2
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-03-08 16:02:59 -0300

    Update mpconfigboard.cmake

--- GENERIC_S3_N16R8/mpconfigboard.cmake
+++ GENERIC_S3_N16R8/mpconfigboard.cmake
@@ -3,18 +3,9 @@ set(IDF_TARGET esp32s3)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.usb
+    boards/sdkconfig.ble
     boards/GENERIC_S3_N16R8/sdkconfig.spiram_sx
     boards/GENERIC_S3_N16R8/sdkconfig.board
-)
-
-set(IDF_TARGET esp32s3)
-
-set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    boards/sdkconfig.usb
-    boards/sdkconfig.ble
-    boards/sdkconfig.spiram_sx
-    boards/ESP32_GENERIC_S3/sdkconfig.board
 )
 
 if(MICROPY_BOARD_VARIANT STREQUAL "SPIRAM_OCT")
