ff96b49c1d397ac834c2d303321d9b487546e9bf
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-05-25 16:48:46 -0300

    Update mpconfigboard.cmake

--- GENERIC_S3_N16R8/mpconfigboard.cmake
+++ GENERIC_S3_N16R8/mpconfigboard.cmake
@@ -2,20 +2,16 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
+    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
     boards/sdkconfig.usb
     boards/sdkconfig.ble
     boards/GENERIC_S3_N16R8/sdkconfig.spiram_sx
     boards/GENERIC_S3_N16R8/sdkconfig.board
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.spiram_oct
 )
 
-if(MICROPY_BOARD_VARIANT STREQUAL "SPIRAM_OCT")
-    set(SDKCONFIG_DEFAULTS
-        ${SDKCONFIG_DEFAULTS}
-        boards/sdkconfig.240mhz
-        boards/sdkconfig.spiram_oct
-    )
-
-    list(APPEND MICROPY_DEF_BOARD
+list(APPEND MICROPY_DEF_BOARD
         MICROPY_HW_BOARD_NAME="Generic ESP32S3 module with Octal-SPIRAM"
     )
 endif()
